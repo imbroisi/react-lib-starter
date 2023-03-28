d2beb5d33ed9e2443002e35a0ab41fa6e550140e
Luiz Imbroisi <luizimbroisi@gmail.com>
2023-03-28 06:08:21 -0300

    add: lint-staged

--- ____lint-staged.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [
-    'eslint --max-warnings=0',
-    // 'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-    () => 'tsc-files --noEmit',
-  ],
-  // '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
-};
