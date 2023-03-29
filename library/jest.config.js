f1352731ea8620804079006251fee76aad6431e9
Luiz Imbroisi <luizimbroisi@gmail.com>
2023-03-29 10:19:03 -0300

    add: test

--- library/jest.config.js
+++ library/jest.config.js
@@ -1,18 +1,13 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   roots: ['<rootDir>/src'],
-
-  // testEnvironment: 'jsdom',
-
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect',
   ],
-
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
